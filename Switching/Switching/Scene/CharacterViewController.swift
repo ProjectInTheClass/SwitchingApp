26a6aa90d525e7842279ad5bcc3876d0235d4107
jngsjn <firstarting@gmail.com>
2020-10-04 05:30:19 +0900

    fix autolayout error, add settingScene
    임시보관함에서 refresh 시 UIImagView 크기 오류 수정.
    Setting view Controler 삭제, 새로 생성

--- Switching/Switching/Scene/CharacterViewController.swift
+++ Switching/Switching/Scene/CharacterViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 class CharacterViewController: UIViewController {
     
     @IBOutlet var characterButtons: [UIButton]!
+    @IBAction func characterButtonClicked(_ sender: UIButton) {
+        let selectedButton = sender.tag
+        for button in self.characterButtons {
+            if (button==sender) {
+                button.alpha = 1
+            } else {
+                button.alpha = 0.6
+            }
+        }
+        print("\(#function) \(selectedButton)")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         for button in self.characterButtons {
             button.layer.cornerRadius = button.frame.width/2
-            button.alpha = 0.5
+            button.alpha = 0.6
         }
 
        
