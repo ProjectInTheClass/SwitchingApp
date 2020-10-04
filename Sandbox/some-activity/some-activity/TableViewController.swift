f61d8f0c6705c8cb9e5826dfc8aea92b082fdf5f
Sanghun Park <po4865@hotmail.com>
2020-10-04 11:23:34 +0900

    edited tableView

--- Sandbox/some-activity/some-activity/TableViewController.swift
+++ Sandbox/some-activity/some-activity/TableViewController.swift
@@ -7,35 +7,46 @@
 
 import UIKit
 
+struct Person {
+    var name: String = ""
+    var age: Int = 0
+    var bloodType: String = ""
+}
+
+let personArray = [Person(name: "Paul", age: 20)]
+
+
+
 class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+         self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+         self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return personArray.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BasicCell", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = personArray[indexPath.row].name
 
         return cell
     }
