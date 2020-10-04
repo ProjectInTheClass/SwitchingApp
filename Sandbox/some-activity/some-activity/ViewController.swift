81ce5e3ac5750e2bfd8f38a251714ace60fe1af6
Sanghun Park <po4865@hotmail.com>
2020-10-04 11:02:49 +0900

    added some-activity project in Sandbox

---
+++ Sandbox/some-activity/some-activity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  some-activity
+//
+//  Created by Sanghun Park on 2020/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
