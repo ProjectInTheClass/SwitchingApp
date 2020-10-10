a7f761265258b6a66f2184b36532c8dac5f2d309
김기훈 <kihoon@inhyejeong.local>
2020-10-10 11:07:16 +0900

    add Page2
    Signed-off-by: 김기훈 <kihoon@inhyejeong.local>

--- Sandbox/week10.playground/Pages/Page 2.xcplaygroundpage/Contents.swift
+++ Sandbox/week10.playground/Pages/Page 2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,29 @@
 
 import Foundation
 
-var str = "Hello, playground"
+let data = ["A big fish in a little pond",
+            "A barking dog never bites",
+            "A good medicine tastes bitter",
+            "A burnt child dreads the fire",
+            "No pain No gain",
+            "Walls have ears",
+            "The more, the better",
+            "A bad workman blames his tools",
+            "A drowning man will catch at a straw",
+            "Do not count your chickens before they hatch",
+            "Beggars can't be choosers",
+            "Between a rock and a hard place",
+            "Blood is thicker than water",
+            "Casting pearls before swine",
+            "Charity begins at home",
+            "Even Homer sometimes nods",
+            "Every cloud has a silver lining"]
 
-//: [Next](@next)
+var count = 0
+
+for proverb in data{
+    if(proverb.contains("the")){
+        count += 1
+    }
+}
+print(count)
