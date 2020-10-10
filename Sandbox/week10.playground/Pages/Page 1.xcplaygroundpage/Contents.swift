bd53ec7cd30e2cde388caadbf0fbbfa55f4bcfe8
jngsjn <firstarting@gmail.com>
2020-10-10 11:04:56 +0900

    add page 1

--- Sandbox/week10.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
+++ Sandbox/week10.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var str = "Hello, playground"
+struct Student{
+    let name: String
+    let age: Int
+    let attendence: Bool
+}
+
+var students = [
+    Student(name: "Jane", age: 12, attendence: true),
+    Student(name: "Minsu", age: 13, attendence: false),
+    Student(name: "July", age: 10, attendence: true),
+    Student(name: "Tom", age: 12, attendence: false),
+    Student(name: "Mary", age: 10, attendence: true),
+    Student(name: "David", age: 11, attendence: true)]
+
+//Q.전체 학생의 이름?
+for name in students {
+    
+}
+
+func attendedStudents () -> Array<String>{
+    return students.map{$0.name}
+}
+print (attendedStudents())
+
+//Q 나이의 총 합?
