94220171bd3387375bbb1360d8872e40ed9e6c9f
Sanghun Park <po4865@hotmail.com>
2020-10-10 11:03:04 +0900

    upload page 3

--- Sandbox/week10.playground/Pages/Page 3.xcplaygroundpage/Contents.swift
+++ Sandbox/week10.playground/Pages/Page 3.xcplaygroundpage/Contents.swift
@@ -1,7 +1,35 @@
 //: [Previous](@previous)
 
-import Foundation
+// Page 3 - Q 1
+var items1: [Int] = [1,4,6,2,33,2,1]
+func reduceCopy_1(array:[Int]) -> Int {
+    
+    var result: Int = 0
+    
+    for item in array {
+        result += item
+    }
+    
+    return result
+}
+var answer1 = reduceCopy_1(array: items1)
 
-var str = "Hello, playground"
+
+// Page 3 - Q 2
+var items2: [String] = [
+    "12345", "King", "Wang", "The", "Best"
+]
+func reduceCopy_2(array:[String]) -> Int {
+    
+    var result: Int = 0
+    
+    for item in array {
+        result += item.count
+    }
+    
+    return result
+}
+var answer2 = reduceCopy_2(array: items2)
+
 
 //: [Next](@next)
