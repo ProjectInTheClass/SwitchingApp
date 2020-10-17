42cfd699afc6d249f7c5e7d02612ebe5e1a43f8d
김기훈 <kihoon@inhyejeong.local>
2020-10-17 14:03:30 +0900

    AccountViewController: use account data from Realm DB
    Signed-off-by: 김기훈 <kihoon@inhyejeong.local>

--- Switching/Switching/Scene/AccountScene/AccountViewController.swift
+++ Switching/Switching/Scene/AccountScene/AccountViewController.swift
@@ -8,35 +8,9 @@
 import UIKit
 
 class AccountViewController: UIViewController {
-    
-    struct Account {
-        var name: String
-        var image = UIImage(named: "account1.png")
-    }
-    var accounts = [Account(name: "프로일잘러"),
-                    Account(name: "프로집순이")]
-    
     @IBAction func cancelClicked(_ sender: UIButton) {
           self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
-//    @IBAction func characterButtonClicked(_ sender: UIButton) {
-//        let selectedButton = sender.tag
-//        for button in self.characterButtons {
-//            if (button==sender) {
-//                button.alpha = 1
-//            } else {
-//                button.alpha = 0.6
-//            }
-//        }
-//        print("\(#function) \(selectedButton)")
-//        if selectedButton == 0{
-//            SharedData.instance.selectedCharacter = "main"
-//        }
-//        else if selectedButton == 1{
-//            SharedData.instance.selectedCharacter = "sub"
-//        }
-//        NotificationCenter.default.post(name: Notification.Name("characterChanged"), object: characterButtons)
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,15 +19,18 @@ class AccountViewController: UIViewController {
 
 extension AccountViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return accounts.count + 1
+        let realm  = SharedData.instance.realm
+        return realm.objects(Character.self).count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cells : UICollectionViewCell?
-        if indexPath.row < accounts.count {
+        let realm  = SharedData.instance.realm
+        if indexPath.row < realm.objects(Character.self).count {
+            let character = realm.objects(Character.self)[indexPath.row]
             let existingCell = collectionView.dequeueReusableCell(withReuseIdentifier: "accountCell", for: indexPath) as! AccountCollectionViewCell
-            existingCell.accountNameLabel.text = accounts[indexPath.row].name
-            existingCell.accountImage.image = accounts[indexPath.row].image
+            existingCell.accountNameLabel.text = character.character
+            existingCell.accountImage.image = UIImage(named: "account1.png")
             existingCell.accountImage.layer.cornerRadius = 50
             cells = existingCell
         } else {
@@ -64,10 +41,12 @@ extension AccountViewController: UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if indexPath.row == accounts.count {
+        let realm  = SharedData.instance.realm
+        if indexPath.row == realm.objects(Character.self).count {
             print("추가하기")
         } else {
             print("버튼이 클릭됨 \(indexPath.row)")
+            SharedData.instance.selectedCharacter = realm.objects(Character.self)[indexPath.row].character
         }
     }
 }
