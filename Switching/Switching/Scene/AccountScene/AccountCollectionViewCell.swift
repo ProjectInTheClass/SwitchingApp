f81528adaf1006b733666412267f4c659acf904a
jngsjn <firstarting@gmail.com>
2020-11-07 22:41:19 +0900

    AccountViewController: edit Edit Mode

--- Switching/Switching/Scene/AccountScene/AccountCollectionViewCell.swift
+++ Switching/Switching/Scene/AccountScene/AccountCollectionViewCell.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class AccountCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var accountView: UIView!
     @IBOutlet weak var accountImage: UIImageView!
     @IBOutlet weak var accountNameLabel: UILabel!
     @IBOutlet weak var editImage: UIImageView!
+    
+    func updateUI() {
+        self.accountView.layer.cornerRadius = self.accountView.frame.height/2
+        self.accountView.clipsToBounds = true
+    }
 }
