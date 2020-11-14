859d23f994b7f529b8c70d5eed8dd459546a8b3c
Kihoon0716 <skfk3606@naver.com>
2020-11-14 22:48:28 +0900

    OnboardingViewControlelr: 더미데이터 생성 폼 만들기
    Signed-off-by: Kihoon0716 <skfk3606@naver.com>

--- Switching/Switching/Scene/OnboardingScene/OnboardingViewController.swift
+++ Switching/Switching/Scene/OnboardingScene/OnboardingViewController.swift
@@ -66,7 +66,73 @@ class OnboardingViewController: UIViewController, onboardingPageViewControllerDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 더미데이터를 사용하지 않는 경우 아래 라인 모두 주석처리
+        
+        let realm = SharedData.instance.realm
 
+        //  여기에 캐릭터 데이터 입력 !!
+        let characters = [
+            ["main", "account1"],
+            ["sub1", "account1"],
+            ["sub2", "account1"],
+            ["sub3", "account1"],
+        ]
+        
+        // 여기에 북마크 데이터 입력 !!
+        
+        let feedBookmarks = [
+            ["naver.com", "naver", "main", ["tag1", "tag2", "tag3"]],
+            ["google.com", "google", "sub1", ["tag1"]],
+            ["apple.com", "apple", "sub2", []],
+        ]
+        
+        let darftBookmarks = [
+            ["https://www.daum.net/", "daum", "main"],
+            ["https://www.youtube.com/", "youtube", "sub1"],
+        ]
+
+        
+        do{
+            try realm.write{
+                for i in 0...characters.count - 1{
+                    let newCharacter = Character()
+                    newCharacter.character = characters[i][0]
+                    newCharacter.image = UIImage(named: characters[i][1])?.pngData()
+                    realm.add(newCharacter)
+                    print("추가")
+                }
+                
+                for i in 0...feedBookmarks.count - 1{
+                    let newBookmark = Bookmark()
+                    newBookmark.url = feedBookmarks[i][0] as! String
+                    newBookmark.desc = feedBookmarks[i][1] as! String
+                    newBookmark.character = feedBookmarks[i][2] as! String
+                    newBookmark.isTemp = false
+                    for tag in feedBookmarks[i][3] as! [String]{
+                        let newTag = Tag()
+                        newTag.tag = tag as! String
+                        newBookmark.tags.append(newTag)
+                    }
+                    realm.add(newBookmark)
+                }
+                
+                for i in 0...darftBookmarks.count - 1{
+                    let newBookmark = Bookmark()
+                    newBookmark.url = darftBookmarks[i][0]
+                    newBookmark.desc = darftBookmarks[i][1]
+                    newBookmark.character = darftBookmarks[i][2]
+                    newBookmark.isTemp = true
+                    realm.add(newBookmark)
+                }
+            }
+        } catch {
+            print("Error Add \(error)")
+        }
+        
+        
+        
+        NotificationCenter.default.post(name: Notification.Name("newCharacterCreated"), object: nil)
         // Do any additional setup after loading the view.
     }
 
