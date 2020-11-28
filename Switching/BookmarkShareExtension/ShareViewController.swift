f6b15bbd86ea0d07d6b0543eac12273ea3c1fd9d
jngsjn <firstarting@gmail.com>
2020-11-29 01:33:41 +0900

    print publicURL & propertylist

--- Switching/BookmarkShareExtension/ShareViewController.swift
+++ Switching/BookmarkShareExtension/ShareViewController.swift
@@ -51,9 +51,33 @@ extension ShareViewController: UICollectionViewDelegate, UICollectionViewDataSou
 
         print("버튼이 클릭됨 \(indexPath.row)")
         var bookmark: Bookmark?
-        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
-            bookmark = accessWebpageProperties(extensionItem: item, characterName: characters[indexPath.row].character)
+        if let inputItems = self.extensionContext?.inputItems,
+           let inputItem = inputItems.first as? NSExtensionItem,
+           let attachments = inputItem.attachments,
+           let attachment = attachments.first {
+            if attachment.hasItemConformingToTypeIdentifier("public.url" as String) {
+                // extension is being called e.g. from Mail app
+                attachment.loadItem(forTypeIdentifier: "public.url" as String, options: nil) { (data, error) in
+                    if let sourceURL = data as? URL {
+                        print("\(sourceURL)")
+                    }
+                }
+            } else {
+                attachment.loadItem(forTypeIdentifier: kUTTypePropertyList as String, options: nil) { (data, error) in
+                    guard let dictionary = data as? NSDictionary,
+                        let results = dictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary,
+                        let url = results["URL"] as? String,
+                        let desc = results["title"] as? String else {
+                            return
+                        }
+                    print("\(dictionary)")
+                }
+            }
         }
+//        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+//
+//            bookmark = accessWebpageProperties(extensionItem: item, characterName: characters[indexPath.row].character)
+//        }
         bookmark?.character = characters[indexPath.row].character
         usleep(1000 * 20)
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
@@ -62,43 +86,42 @@ extension ShareViewController: UICollectionViewDelegate, UICollectionViewDataSou
     }
 }
 
-private func accessWebpageProperties(extensionItem: NSExtensionItem, characterName: String) -> Bookmark{
-        // url 가져오기
-        let propertyList = kUTTypePropertyList as String
-        var bookmark = Bookmark()
-
-        for attachment in extensionItem.attachments! where attachment.hasItemConformingToTypeIdentifier(propertyList) {
-            attachment.loadItem(
-                forTypeIdentifier: propertyList,
-                options: nil,
-                completionHandler: { (item, error) -> Void in
-
-                    guard let dictionary = item as? NSDictionary,
-                        let results = dictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary,
-                        let url = results["URL"] as? String,
-                        let desc = results["title"] as? String else {
-                            return
-                        }
-
-                    print("url: \(url)")
-                    bookmark.url = url
-                    print("desc: \(desc)")
-                    bookmark.desc = desc
-                    bookmark.character = characterName
-                    print("character: \(bookmark.character)")
-
-                    usleep(1000 * 10)
-                    let realm = SharedData.instance.newRealm()
-                    do{
-                        try realm.write{ // realm.write{}는 git에서 commit을 해주는 것과 비슷하다.
-                            realm.add(bookmark) // 데이터베이스에 park 모델을 더한다.
-                        }
-                    } catch {
-                        print("Error Add \(error)")
-                    }
-                    print("add data done")
-                }
-            )
-        }
-        return bookmark
-    }
+//private func accessWebpageProperties(extensionItem: NSExtensionItem, characterName: String) -> Bookmark{
+//        // url 가져오기
+//    let propertyList = kUTTypePropertyList as String
+//    var bookmark = Bookmark()
+//        for attachment in extensionItem.attachments! where attachment.hasItemConformingToTypeIdentifier(propertyList) {
+//            attachment.loadItem(
+//                forTypeIdentifier: propertyList,
+//                options: nil,
+//                completionHandler: { (item, error) -> Void in
+//
+//                    guard let dictionary = item as? NSDictionary,
+//                        let results = dictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary,
+//                        let url = results["URL"] as? String,
+//                        let desc = results["title"] as? String else {
+//                            return
+//                        }
+//
+//                    print("url: \(url)")
+//                    bookmark.url = url
+//                    print("desc: \(desc)")
+//                    bookmark.desc = desc
+//                    bookmark.character = characterName
+//                    print("character: \(bookmark.character)")
+//
+//                    usleep(1000 * 10)
+//                    let realm = SharedData.instance.newRealm()
+//                    do{
+//                        try realm.write{ // realm.write{}는 git에서 commit을 해주는 것과 비슷하다.
+//                            realm.add(bookmark) // 데이터베이스에 park 모델을 더한다.
+//                        }
+//                    } catch {
+//                        print("Error Add \(error)")
+//                    }
+//                    print("add data done")
+//                }
+//            )
+//        }
+//        return bookmark
+//    }
